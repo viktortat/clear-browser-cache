0b5e5b334bab523920bf68712f61b58f180ade5a
Cory Bateman <splintercode.cb@gmail.com>
2014-10-15 12:52:21 -0500

    Added iphone gif and new css work

--- App/JavaScript/Config/bundle.js
+++ App/JavaScript/Config/bundle.js
@@ -523,63 +523,67 @@ this.current.$$route){var c={},f=this;e.forEach(Object.keys(a),function(b){f.cur
 
     appControllers.controller('BaseCtrl', function () {
         var vm = this;
+        var browserDetail = bowser;
+        vm.browser = getBrowser();
+        vm.bowser = bowser;
 
-        vm.browser = bowser;
-        vm.image = getImage();
-        vm.video = getVideo();
-
-        function getImage() {
+        function getBrowser() {
             // http://onelittledesigner.com/rapidweaver/web-icons/free-flat-browser-icons/
-            var browserImage = '';
+            var imagePrefix = 'Content/Images/';
+            var videoPrefix = 'Content/Images/Gifs/';
+            var browser = {};
 
-            switch (bowser.name) {
-                case "Chrome":
-                    browserImage = 'Chrome.png';
-                    break;
-                case "Internet Explorer":
-                    browserImage = 'InternetExplorer.png';
-                    break;
-                case "Firefox":
-                    browserImage = 'Firefox.png';
-                    break;
-                case "Opera":
-                    browserImage = 'Opera.png';
-                    break;
-                case "Safari":
-                    browserImage = 'Safari.png';
-                    break;
-                default:
-                    browserImage = '';
+            if (browserDetail.name === 'Android' && browserDetail.android === true) {
+                browser.image = 'Chrome.png';
+                browser.video = 'Android-4.2-default.gif';
+                browser.name = 'Android';
+            } else if (browserDetail.name === 'Chrome' && browserDetail.android === true) {
+                browser.image = 'Chrome.png';
+                browser.video = 'Android-4.2-default.gif';
+                browser.name = 'Chrome on Android';
+            } else if (browserDetail.name === 'Chrome') {
+                browser.image = 'Chrome.png';
+                browser.video = 'Chrome-38.gif';
+                browser.name = 'Chrome';
+            } else if (browserDetail.name === 'Internet Explorer') {
+                browser.image = 'InternetExplorer.png';
+                browser.video = 'InternetExplorer-11.gif';
+                browser.name = 'Internet Explorer';
+            } else if (browserDetail.name === 'Firefox') {
+                browser.image = 'Firefox.png';
+                browser.video = 'Firefox-32.gif';
+                browser.name = 'Firefox';
+
+            } else if (browserDetail.name === 'Opera') {
+                browser.image = 'Opera.png';
+                browser.video = 'Opera-12.gif';
+                browser.name = 'Opera';
+            }
+            else if (browserDetail.name === 'Safari') {
+                browser.image = 'Safari.png';
+                browser.video = 'Safari-8.gif';
+                browser.name = 'Safari';
+            }
+            else if (browserDetail.name === 'iPhone') {
+                browser.image = 'Safari.png';
+                browser.video = 'Safari-ios8-iphone.gif';
+                browser.name = 'Safari on iPhone';
+            }
+            else if (browserDetail.name === 'iPad') {
+                browser.image = 'Safari.png';
+                browser.video = 'Safari-ios8.gif';
+                browser.name = 'Safari on iPad';
+            } else {
+                browser.image = '';
+                browser.video = '';
+                browser.name = '';
             }
 
-            return 'Content/Images/' + browserImage;
-        }
+            browser.image = imagePrefix + browser.image;
+            browser.video = videoPrefix + browser.video;
+            browser.version = browserDetail.version;
 
-        function getVideo() {
-            // http://onelittledesigner.com/rapidweaver/web-icons/free-flat-browser-icons/
-            var browserVideo = '';
-
-            switch (bowser.name) {
-                case "Chrome":
-                    browserVideo = 'Chrome-38.gif';
-                    break;
-                case "Internet Explorer":
-                    browserVideo = 'InternetExplorer-11.gif';
-                    break;
-                case "Firefox":
-                    browserVideo = 'Firefox-32.gif';
-                    break;
-                case "Opera":
-                    browserVideo = 'Opera-12.gif';
-                    break;
-                case "Safari":
-                    browserVideo = 'Safari-8.gif';
-                    break;
-                default:
-                    browserVideo = '';
-            }
-
-            return 'Content/Images/Gifs/' + browserVideo;
+            return browser;
         }
     });
 })();
